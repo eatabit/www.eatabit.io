d37e78180149518be2bdedc00fab6fbc4a014c43
Greg Oleksiak <greg@eatabit.com>
2018-05-15 11:14:16 -0400

    Begin NRA redesign

---
+++ assets/javascripts/connections.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function() {
+  jQuery('#cta-1').connections({ to: '#logo-1', 'class': 'connection-red' });
+  jQuery('#cta-2').connections({ to: '#logo-2', 'class': 'connection-orange' });
+  jQuery('#cta-3').connections({ to: '#logo-3', 'class': 'connection-yellow' });
+  jQuery('#cta-4').connections({ to: '#logo-4', 'class': 'connection-green' });
+  jQuery('#cta-5').connections({ to: '#logo-5', 'class': 'connection-blue' });
+  jQuery('#cta-6').connections({ to: '#logo-6', 'class': 'connection-purple' });
+});
