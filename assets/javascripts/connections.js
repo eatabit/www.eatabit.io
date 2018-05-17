52d6e62fce0cb1ca7fe6bf0f132abb2f4e2786c9
Greg Oleksiak <greg@eatabit.com>
2018-05-17 19:41:03 -0400

    fixes

--- assets/javascripts/connections.js
+++ assets/javascripts/connections.js
@@ -1,10 +1,10 @@
 jQuery(document).ready(function() {
-  jQuery('#printer').connections({ to: '#logo-1', 'class': 'connection-red' });
-  jQuery('#printer').connections({ to: '#logo-2', 'class': 'connection-orange' });
-  jQuery('#printer').connections({ to: '#logo-3', 'class': 'connection-yellow' });
-  jQuery('#printer').connections({ to: '#logo-4', 'class': 'connection-green' });
-  jQuery('#printer').connections({ to: '#logo-5', 'class': 'connection-blue' });
-  jQuery('#printer').connections({ to: '#logo-6', 'class': 'connection-purple' });
+  jQuery("#printer").connections({ to: "#logo-1", "class": "connection-red" });
+  jQuery("#printer").connections({ to: "#logo-2", "class": "connection-orange" });
+  jQuery("#printer").connections({ to: "#logo-3", "class": "connection-yellow" });
+  jQuery("#printer").connections({ to: "#logo-4", "class": "connection-green" });
+  jQuery("#printer").connections({ to: "#logo-5", "class": "connection-blue" });
+  jQuery("#printer").connections({ to: "#logo-6", "class": "connection-purple" });
 
-  jQuery(".row.printer").css('padding-top', '2rem');
+  jQuery(".row.printer").css("padding-top", "2rem");
 });
